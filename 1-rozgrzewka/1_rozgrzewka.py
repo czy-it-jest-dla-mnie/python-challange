e15c9fd7576b682a85a3ebed567d12b00af35b36
Filip Paluch <filip.paluch@o2.pl>
2022-03-29 15:50:59 +0200

    Add files via upload

---
+++ 1-rozgrzewka/1_rozgrzewka.py
@@ -0,0 +1,41 @@
+ #1. Wypisz tekst na konsoli 
+ 
+tekst = "Hello World!"
+ 
+
+#2. Dodaj dwie liczby i wyświetl na konsoli
+
+a = 5
+b = 10
+
+#3. Przypisz tekst "test" do trzech zmiennych (x,y,z) za pomocą jednej komendy ( w jednej linijce kodu )
+
+#4. Skonwertuj liczbę 6.5 do liczy całkowitej i przypisz do zmiennej d i wypisz
+
+c = 6.5
+
+#5. Wypisz ilość znaków w tekście "Hello World"
+
+e = "Hello World"
+
+#6. Wypisz pierwszy znak z tekstu "Hello World" - używając zmiennej e
+
+#7. Usuń spacje z napisu "Hello  World " i wypisz
+
+f = "Hello  World "
+
+#8. Dodaj element "Chevrolet" jako ostatni do listy samochodów 
+
+cars = ["Dodge", "RAM"]
+
+#9. Usuń element "RAM" z listy samochodów
+
+#10. Wypisz 2, 3 i 4 element z listy używając zakresu indeksów
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+#11. Wypisz wielkość tablicy 'fruits'
+
+#12. Stwórz funkcję która wypisuje podany w parametrze tekst a następnie ją wywołaj
+
+
